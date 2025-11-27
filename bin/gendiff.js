461783fc54d826e8cd8e326169c9d7d3345f1878
glazunov aleksei <993468@gmail.com>
2025-11-27 14:26:42 +0400

    add format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const { program } = require("commander");
 
 program
+  .version("1.0.0")
   .description("Compares two configuration files and shows a difference.")
-  .version("1.0.0");
+  .argument("<filepath1>")
+  .argument("<filepath2>")
+  .option("-f, --format <type>", "output format");
 
 program.parse(process.argv);
